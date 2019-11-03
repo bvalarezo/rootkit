4200f1ff3f1259be940a838a0f9d443fd7737bc5
bdefino <bailey.defino@stonybrook.edu>
2019-11-02 21:51:46 -0400

    restructured: now the module is expected to be compiled with hooks

--- sctm.c
+++ sctm.c
@@ -2,24 +2,30 @@
 
 /* system call table modification module */
 
-struct sctm_hook hook = (struct sctm_hook) {
-  .original = NULL
+struct sctm_hook_lstack *hooked_table[SCTM_TABLE_SIZE] = {
+  [0 ... SCTM_TABLE_SIZE] NULL
 };
-unsigned long _hook_call = -1;
-sctm_syscall_handler_t _hook_hook = NULL;
-enum sctm_unhook_mode _hook_unhook_mode = SCTM_UNHOOK_MODE_SOFT;
-static struct sctm_hook *sctm__hook_wrapper_table[SCTM_TABLE_SIZE];
+
 sctm_syscall_handler_t *table = NULL;
 
 static void __exit sctm__exit(void) {
-  /* unhook the call */
+  /* unhook all hooked system calls (and ignore failure) */
 
-  sctm__unhook(table, &hook);
+  sctm__unhook_all(table, hook_stack);
+
+  /* call the exit hook (if any) */
+
+  return SCTM__EXIT_HOOK();
 }
 
-static int sctm__hook(sctm_syscall_handler_t *table, struct sctm_hook *hook) {
-  int retval;
+int sctm_hook(struct sctm_hook *hook) {
+  return 0;//////////////////////////////////////////////
+}
 
+static int sctm__hook(struct sctm_hook *hook) {
+  struct sctm_hook_lstack *node;
+  int retval;
+  
   if (hook == NULL)
     return -EINVAL;
   
@@ -31,16 +37,19 @@ static int sctm__hook(sctm_syscall_handler_t *table, struct sctm_hook *hook) {
 
   if (table == NULL)
     return -EFAULT;
+
+  /* hook */
+
   hook->original = table[hook->call];
   retval = sctm__set_syscall_handler(table, hook->call, hook->hook);
 
   if (retval)
     return retval;
   hook->hooked = ~0;
-  sctm__hook_wrapper_table[hook->call] = hook; /* MUST be done last */
   return 0;
 }
 
+/////////////////////////////////////possibly rewrite, but add differentiation for multiple hooks on the same syscall (or not?)
 asmlinkage long sctm__hook_wrapper(unsigned long call, unsigned long arg0, unsigned long arg1, unsigned long arg2, unsigned long arg3, unsigned long arg4) {
   struct sctm_hook *hook;
   int retval;
@@ -79,18 +88,14 @@ asmlinkage long sctm__hook_wrapper(unsigned long call, unsigned long arg0, unsig
 };
 
 static int __init sctm__init(void) {
-  struct sctm_hook **cur;
-  int i;
   int retval;
 
-  /* load the parameters */
+  /* call the initialization hook (if any) */
 
-  hook.call = _hook_call;
-  hook.hook = _hook_hook;
-  hook.unhook_mode = _hook_unhook_mode;
+  retval = SCTM__INIT_HOOK();
 
-  if (hook.call >= SCTM_TABLE_SIZE)
-    return -EINVAL;
+  if (retval)
+    return retval;
 
   /* locate the syscall table */
 
@@ -98,50 +103,42 @@ static int __init sctm__init(void) {
 
   if (retval)
     return retval;
-
-  /* clear the hook table */
-
-  for (cur = sctm__hook_wrapper_table, i = SCTM_TABLE_SIZE; i; i++)
-    *cur = NULL;
-  return sctm__hook(table, &hook);
 }
 
-static int sctm__locate_sys_call_table(sctm_syscall_handler_t **dest) {
-#ifndef CONFIG_KALLSYMS
-  void *max_ptr;
-#endif
-  if (dest == NULL)
-    return -EFAULT;
+static int sctm__locate_sys_call_table(void) {
 #if CONFIG_KALLSYMS
   /* kernel symbols are accessible */
 
-  *dest = (sctm_syscall_handler_t *) kallsyms_lookup_name("sys_call_table");
+  table = (sctm_syscall_handler_t *) kallsyms_lookup_name("sys_call_table");
 #else
+  void *max_ptr;
   /* iteratively detect for the system call table (it's cache-aligned) */
 
   max_ptr = (void *) ~0;
 
-  for (*dest = 0; (void *) *dest < max_ptr && **dest != (sctm_syscall_handler_t) &SCTM_TABLE_FIRST_HANDLER; *dest += SMP_CACHE_BYTES);
+  for (table = 0; (void *) table < max_ptr && **dest != (sctm_syscall_handler_t) &SCTM_TABLE_FIRST_HANDLER; table += SMP_CACHE_BYTES);
 
-  if ((void *) *dest == max_ptr)
-    *dest = NULL;
+  if ((void *) table == max_ptr)
+    table = NULL;
 #endif
-  if (*dest == NULL)
+  if (table == NULL)
     return -EFAULT;
   return 0;
 }
 
-static int sctm__set_syscall_handler(sctm_syscall_handler_t *table, unsigned long call, sctm_syscall_handler_t handler) {
-  if (table == NULL)
-    return -EFAULT;
-
+static int sctm__set_syscall_handler(unsigned long call, sctm_syscall_handler_t handler) {
   if (call >= SCTM_TABLE_SIZE)
     return -EINVAL;
   /////////////////////////////////////////////////////////////////
   return 0;
 }
 
-static int sctm__unhook(sctm_syscall_handler_t *table, struct sctm_hook *hook) {
+int sctm_unhook(struct sctm_hook *hook) {
+  //////////////////////////////////////////////////////
+}
+
+////////////////////////////////////rewrite and add compatibility for the linked hook table
+static int sctm__unhook(struct sctm_hook *hook) {
   int retval;
 
   if (hook == NULL)
--- sctm.h
+++ sctm.h
@@ -11,6 +11,12 @@
 
 /* system call table modification module */
 
+#ifndef SCTM__EXIT_HOOK
+#define SCTM__EXIT_HOOK() 0 /* module exit hook */
+#endif
+#ifndef SCTM__INIT_HOOK
+#define SCTM__INIT_HOOK() 0 /* module initialization hook */
+#endif
 #ifndef SCTM_TABLE_FIRST_HANDLER
 #define SCTM_TABLE_FIRST_HANDLER sys_read /* x86-64 */
 #endif
@@ -21,42 +27,39 @@
 /* convenient type alias */
 typedef asmlinkage long (*sctm_syscall_handler_t)(unsigned long, unsigned long, unsigned long, unsigned long, unsigned long, unsigned long);
 
-/* unhook modes */
-enum sctm_unhook_mode {
-  SCTM_UNHOOK_MODE_HARD, /* replace with the original handler and unset the hooked flag */
-  SCTM_UNHOOK_MODE_SOFT /* unset the hooked flag and assume the handler will only provide original functionality */
+/* unhook methods */
+enum sctm_unhook_method {
+  SCTM_UNHOOK_METHOD_DISABLE, /* disable the hook by instead calling the original */
+  SCTM_UNHOOK_METHOD_REPLACE /* replace the system call table entry with its original value */
 };
 
 /* hooked system call */
 struct sctm_hook {
-  unsigned long call;
-  sctm_syscall_handler_t hook;
+  unsigned long call; /* system call number (index within system call table) */
+  sctm_syscall_handler_t hook; /* hook function */
   int hooked; /* flag for whether the hook should provide new functionality */
-  enum sctm_unhook_mode unhook_mode;
+  enum sctm_unhook_method unhook_method; /* how to unhook the system call */ 
   sctm_syscall_handler_t original;
 };
 
-//////////////////////////////////////////////wrapper table is limited to 1 hook per call:
-///////////////////////////////////////////////this is the workaround basei (not yet integrated)
-/* linked list of hooks */
-struct sctm_hook_list {
+/* linked stack of hooked system calls */
+struct sctm_hook_lstack {
   struct sctm_hook *hook;
-  struct sctm_hook_list *next;
+  struct sctm_hook_stack *next;
 };
 
-struct sctm_hook hook;
-unsigned long _hook_call;
-sctm_syscall_handler_t _hook_hook;
-enum sctm_unhook_mode _hook_unhook_mode;
-static struct sctm_hook *sctm__hook_wrapper_table[SCTM_TABLE_SIZE]; /* workaround for not being able to pass a hook to sctm__hook_wrapper */
+struct sctm_hook_lstack *hooked_table[SCTM_TABLE_SIZE]; /* quick lookup for hooked system calls */
 extern sctm_syscall_handler_t SCTM_TABLE_FIRST_HANDLER;
 sctm_syscall_handler_t *table;
 
 /* module cleanup */
 static void __exit sctm__exit(void);
 
-/* hook the system call table */
-static int sctm__hook(sctm_syscall_handler_t *table, struct sctm_hook *hook);
+/* hook and register a system call */
+int sctm_hook(struct sctm_hook *hook);
+
+/* hook a system call */
+static int sctm__hook(struct sctm_hook *hook);
 
 /* call the hook and/or the original */
 asmlinkage long sctm__hook_wrapper(unsigned long call, unsigned long arg0, unsigned long arg1, unsigned long arg2, unsigned long arg3, unsigned long arg4);
@@ -65,21 +68,24 @@ asmlinkage long sctm__hook_wrapper(unsigned long call, unsigned long arg0, unsig
 static int __init sctm__init(void);
 
 /* locate the system call table */
-static int sctm__locate_sys_call_table(sctm_syscall_handler_t **dest);
+static int sctm__locate_sys_call_table(void);
 
 /* set a system call handler */
-static int sctm__set_syscall_handler(sctm_syscall_handler_t *table, unsigned long call, sctm_syscall_handler_t handler);
+static int sctm__set_syscall_handler(unsigned long call, sctm_syscall_handler_t handler);
 
-/* unhook a system call */
-static int sctm__unhook(sctm_syscall_handler_t *table, struct sctm_hook *hook);
+/* unhook and possibly deregister a hooked system call */
+int sctm_unhook(struct sctm_hook *hook);
+
+/* unhook a hooked system call */
+static int sctm__unhook(struct sctm_hook *hook);
+
+/* unhook and deregister all hooked system calls */
+int sctm_unhook_all(void);
 
 MODULE_LICENSE("GPL");
 
 module_exit(sctm__exit);
 module_init(sctm__init);
-
-/*module_param(_hook_call, int, 0x0700);
-module_param(_hook_hook, sctm_syscall_handler_t, 0x0700);*/
 
 #endif
 
